8878d42caf40a889c7d73f334732f7683f6e8758
xpc <xinpeicheng@yeah.net>
2018-02-27 00:25:14 +0800

    00:25

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ const Koa = require('koa')
 const app = new Koa()
 const views = require('koa-views')
 const { resolve } = require('path')
+const serve = require('koa-static')
+
+app.use(serve(__dirname+'/views'))
+
 app.use(views(resolve(__dirname,'./views'),{
     'extension':'pug'
 }))
 app.use(async (ctx,next)=>{
     await ctx.render('index.pug',{
         'you':'Luke',
-        'me':'辛培铖'
     })
 })
 
